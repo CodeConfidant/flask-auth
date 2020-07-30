c10d4e86c8cc05afc37f431cbb94ecb3b04f556d
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-07-30 17:16:47 -0400

    Updated DB model

--- app/db-model.py
+++ app/db-model.py
@@ -4,8 +4,8 @@ from connectwrap import db, utils
 if (os.path.exists("app/users.db") == False):
     utils.create_database("app/users.db")
     user_db = db("app/users.db")
-    user_db.create_table("users", Username="str", Password="str")
-    user_db.insert_row("users", "master", "master")
+    user_db.create_table("users", Email="str", Username="str", Password="str")
+    user_db.insert_row("users", "master@test.com", "master", "master")
 
 elif (os.path.exists("app/users.db") == True):
     user_db = db("app/users.db")
--- app/routes.py
+++ app/routes.py
@@ -18,18 +18,19 @@ def account():
 @app.route('/adduser', methods = ['POST', 'GET'])
 def adduser():
     if (request.method == 'POST'): 
+        email = request.form['email']
         username = request.form['username']
         password = request.form['password']
         user_db = db("app/users.db")
         message = ""
 
-        if (user_db.get_row("users", "Username", username) == None):
-            user_db.insert_row("users", username, password)
-            message = "Account Created!"
+        if (user_db.get_row("users", "Email", email) == None and user_db.get_row("users", "Username", username) == None):
+            user_db.insert_row("users", email, username, password)
+            message = "Account Created! Welcome " + username
             user_db.close_db()
             return render_template("account.html", title='Account', message=message)
         else:
-            message = "Account Creation Failed! That username already exists!"
+            message = "Account Creation Failed! That username or email already exists!"
             user_db.close_db()
             return render_template('register.html', title='Register', message=message)
 
@@ -40,13 +41,18 @@ def loginuser():
         password = request.form['password']
         user_db = db("app/users.db")
         message = ""
+        row = user_db.get_row("users", "Username", username)
 
         try:
-            if (user_db.get_row("users", "Username", username)["Username"] == username and user_db.get_row("users", "Password", password)["Password"] == password):
+            if (row["Username"] == username and row["Password"] == password):
                 message = "Welcome " + username
                 user_db.close_db()
                 return render_template("account.html", title='Account', message=message)
+            else:
+                message = "User Login Failed! Either the username or password is incorrect!"
+                user_db.close_db()
+                return render_template("login.html", title='Login', message=message)
         except:
             message = "User Login Failed! Either the username or password is incorrect!"
             user_db.close_db()
-            return render_template('login.html', title='Login', message=message)
+            return render_template("login.html", title='Login', message=message)
