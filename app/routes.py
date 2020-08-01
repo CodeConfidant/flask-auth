7d1cec5049bf0bda950a2bbaa2091eb580f9a7e4
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-08-01 19:20:38 -0400

    Update base.html meta, added index, updated routes, added support for styles

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from flask import render_template, url_for, request
 from app import app
 
 @app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', title='Home')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html', title='Login')
