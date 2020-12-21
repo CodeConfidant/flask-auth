ddd9e985160f2567d11e9f6e21eab5a7e5a4d4fa
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-12-21 00:20:17 -0500

    Added support for Flask-Forms and cleaned up things.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from passlib.hash import sha512_crypt
 from connectwrap import db
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 from app import app
+from app.forms import LoginForm, RegisterForm
 
 user_db = db("app/users.db", "Users"); user_db.close_db()
 
@@ -12,19 +13,19 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login')
+    return render_template('login.html', title='Sign In', form=LoginForm())
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    return render_template('register.html', title='Register', form=RegisterForm())
 
 @app.route('/account', methods=['GET', 'POST'])
 def account():
     return render_template('account.html', title='Account')
 
-@app.route('/adduser', methods = ['POST', 'GET'])
-def adduser():
-    if (request.method == 'POST'): 
+@app.route('/register_user', methods=['GET', 'POST'])
+def register_user():
+    if (request.method == 'POST'):
         email = request.form['email']
         username = request.form['username']
         password = request.form['password']
@@ -38,8 +39,8 @@ def adduser():
             user_db.close_db()
             return render_template('register.html', title='Register', message="Account Creation Failed! That username or email already exists!")
 
-@app.route('/loginuser', methods = ['POST', 'GET'])
-def loginuser():
+@app.route('/login_user', methods=['GET', 'POST'])
+def login_user():
     if (request.method == 'POST'): 
         username = request.form['username']
         password = request.form['password']
@@ -57,8 +58,8 @@ def loginuser():
             user_db.close_db()
             return render_template("login.html", title='Login', message="User Login Failed! Either the username or password is incorrect!")
 
-@app.route('/deluser')
-def deluser(): 
+@app.route('/del_user')
+def del_user(): 
         username = request.args.get('id')
         user_db.open_db()
         row = user_db.get_row("Username", username)
@@ -70,31 +71,3 @@ def deluser():
             user_db.close_db()
         
             return render_template("index.html", title='Home', message=str("User {0} successfully deleted!").format(username))
-
-@app.route('/changepassword', methods = ['POST', 'GET'])
-def changepassword():
-    if (request.method == 'POST'):
-        username = request.form['username']
-        password = request.form['password']
-        new_password = request.form['new_password']
-        user_db.open_db()
-        row = user_db.get_row("Username", username)
-
-        if (row != None and row["Username"] == username and sha512_crypt.verify(password, row["Password"]) == True):
-            user_db.update_row("Password", sha512_crypt.hash(new_password), "Username", username)
-            user_db.close_db()
-            return render_template("account.html", title='Account', message="Password changed successfully!", email=row["Email"], username=row["Username"], type=row["Type"])
-        else:
-            user_db.close_db()
-            return render_template("account.html", title='Account', message="Password change failed! The current password was incorrect!", email=row["Email"], username=row["Username"], type=row["Type"])
-
-@app.route('/changeusername', methods = ['POST', 'GET'])
-def changeusername():
-    if (request.method == 'POST'):
-        username = request.form['username']
-        new_username = request.form['new_username']
-        user_db.open_db()
-        user_db.update_row("Username", new_username, "Username", username)
-        row = user_db.get_row("Username", new_username)
-        user_db.close_db()
-        return render_template("account.html", title='Account', message="Username changed successfully!", email=row["Email"], username=row["Username"], type=row["Type"])
