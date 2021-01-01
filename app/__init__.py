cae03121e82d9a1845234f6155e1cdff797d21c1
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2021-01-01 18:59:16 -0500

    Updated configuration & Environment Variables

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Make this a random string!'
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
 app.static_folder = 'static'
 
 from app import routes
